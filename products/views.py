2546275649a13584cea8780dea81b1b97a315c6b
hpearson98 <harry.m.pearson@gmail.com>
2023-10-24 18:04:20 +0000

    Added watches view and urls

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Watch
+
+
+def all_watches(request):
+    """
+    This view shows all products including sorting and search queries
+    """
+    watches = Watch.objects.all()
+    context = {
+        'watches': watches,
+    }
+    return render(request, 'watches/watches.html', context)
