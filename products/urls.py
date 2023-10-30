b256e7fb68acd5177bdbf001c5e653af4873a5fb
hpearson98 <harry.m.pearson@gmail.com>
2023-10-30 20:16:59 +0000

    Added product details function

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_watches, name='watches')
+    path('', views.all_watches, name='watches'),
+    path('<watch_id>', views.watch_detail, name='watch_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.shortcuts import render
@@ -7,10 +7,21 @@ from .models import Watch
 
 def all_watches(request):
     """
-    This view shows all products including sorting and search queries
+    This view shows all watches including sorting and search queries
     """
     watches = Watch.objects.all()
     context = {
         'watches': watches,
     }
     return render(request, 'watches/watches.html', context)
+
+
+def watch_detail(request, watch_id):
+    """
+    This view shows the details of the selected watch
+    """
+    watch = get_object_or_404(Watch, pk=watch_id)
+    context = {
+        'watch': watch,
+    }
+    return render(request, 'watches/watch_detail.html', context)
