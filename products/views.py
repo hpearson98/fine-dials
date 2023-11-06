424eccee5e989c03a9dc953e8c373a89eaf6ded5
hpearson98 <harry.m.pearson@gmail.com>
2023-11-06 15:22:11 +0000

    Added functionality to sort by name, price and rating and also added button to jump to top of the products page

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.db.models import Q
+from django.db.models.functions import Lower
 from django.contrib import messages
 
 from .models import Watch, Brand
@@ -12,7 +13,22 @@ def all_watches(request):
     watches = Watch.objects.all()
     query = None
     brands = None
+    sort = None
+    direction = None
 
+    if 'sort' in request.GET:
+        sortkey = request.GET['sort']
+        sort = sortkey
+        if sortkey == 'model':
+            sortkey = 'lower_model'
+            watches = watches.annotate(lower_model=Lower('model'))
+
+        if 'direction' in request.GET:
+            direction = request.GET['direction']
+            if direction == 'desc':
+                sortkey = f'-{sortkey}'
+            
+            watches = watches.order_by(sortkey)
     if 'brand' in request.GET:
         brands = request.GET['brand'].split(',')
         watches = watches.filter(brand__name__in=brands)
@@ -35,10 +51,13 @@ def all_watches(request):
 
             watches = watches.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'watches': watches,
         'search_term': query,
         'current_brands': brands,
+        'current_sorting': current_sorting,
     }
     return render(request, 'watches/watches.html', context)
 
