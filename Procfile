bea2cad49e0c4af412d967d70c472562c4b5871e
hpearson98 <harry.m.pearson@gmail.com>
2023-11-18 14:54:02 +0000

    Changed secret key and stored it in config vars, created external database and deployed to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fine-dials.wsgi:application
--- fine_dials/settings.py
+++ fine_dials/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,13 +21,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*fij1$%7c8k!yyaj$sknrxz2=&xc5a(%f=5r-s%pxb&z@zhafi'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-hpearson98-finedials-ztmk34pm046.ws-eu106.gitpod.io'
+    'https://fine-dials-fd29b5e3eda1.herokuapp.com/'
 ]
 
 
@@ -115,13 +117,25 @@ LOGIN_REDIRECT_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
+"""
+DATABASES = {
+     'default': dj_database_url.parse(
+        'postgres://wpylkwyj:o_B9_ykgGDis27Vu05D9I0_GRyBohqs7@rogue.db.elephantsql.com/wpylkwyj'
+     )
+ }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
